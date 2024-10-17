e1df6c3d912511eff65d1567c487a6a6f124ad3e
Duncan Horne <duncan.edwardhorne@gmail.com>
2024-10-17 10:34:02 +0100

    Initial Commit

---
+++ PokemonTCG/PokemonTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGApp.swift
+//  PokemonTCG
+//
+//  Created by Duncan Horne on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonTCGTests/PokemonTCGTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGTests.swift
+//  PokemonTCGTests
+//
+//  Created by Duncan Horne on 17/10/2024.
+//
+
+import Testing
+@testable import PokemonTCG
+
+struct PokemonTCGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
